8031a79aeb0f50a867537e06bc22dacd2ffe13a2
Luis R Zunniga S luiscr1104@gmail.com <luiscr1104@gmail.com>
2023-03-21 09:55:41 -0600

    Se crear el app para el formulario, se agrega una pag de inicio con el form y se agrega un header

---
+++ src/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { AppBar, Toolbar, Typography, Button } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const Header = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            GrupoEcoquintas
+          </Typography>
+          <Button color="inherit">Inicio</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default Header;
